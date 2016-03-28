6ce6f2428032de062f40bffd85fa096743eee702
“mgoel1” <mgoel@walmartlabs.com>
2016-03-27 20:22:36 -0700

    adding book title to book detail container

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -4,8 +4,16 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+
+    if (!this.props.book) {
+      return <div>Select a book to started.</div>
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>{this.props.book.title}</div>
+      </div>
     )
   }
 }
